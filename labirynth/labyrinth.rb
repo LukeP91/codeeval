5ff37f213bf8b1fb79de25f5e5d39684fa72d295
Luke Pawlik <lukepawlik@gmail.com>
2017-08-27 14:29:24 +0200

    add almost solution

--- labirynth/labyrinth.rb
+++ labirynth/labyrinth.rb
@@ -1,66 +1,97 @@
-require "minitest/autorun"
 require 'pry'
 
-class LabyrinthTest < Minitest::Test
-  def test_that_it_stores_labyrinth_as_array
-  end
-
-  def test_that_it_can_find_start
-    labyrinth = Labyrinth.new('test1.txt')
-    array = Array.new(5) { ['*', '+', '*']}
-    assert_equal labyrinth.start, array
-  end
-end
-
 class Labyrinth
-  attr_reader :schema, :solutions
+  attr_reader :maze
 
   def initialize(path_to_file)
-    @schema = []
+    @maze = []
     File.open(path_to_file).each_line do |file_line|
-      @schema << file_line.chop.split('')
+      @maze << file_line.chop.split('')
     end
   end
 
   def solution
-    @schema.each do |line|
+    @solution = Maze.new(@maze).best_solution
+    mark_solution
+    # print_solution
+    @maze
+  end
+
+  def mark_solution
+    @solution.each do |y, x|
+      @maze[y][x] = '+'
+    end
+  end
+
+  def print_solution
+    @maze.each do |line|
+      puts line.join('')
     end
   end
 end
 
-class SolutionPath
-  def initialize
-    @solution_nodes = []
+class Maze
+  DIRECTIONS = [
+    [0, -1], # DOWN
+    [-1, 0], # LEFT
+    [1, 0], # RIGHT
+    [0, 1] # UP
+  ].freeze
+
+  def initialize(maze)
+    @maze = maze
+    @solutions = []
+    @dim_y = maze.length - 1
+    @dim_x = maze[1].length - 1
+    @start = [0, @maze[0].find_index(' ')]
+    @finish = [@dim_y, @maze[@dim_y].find_index(' ')]
   end
 
-  def current_node
-    @solution_nodes.last
+  def best_solution
+    find_solutions
+    @solutions.min_by(&:length)
   end
 
-  def add_next_node(next_node)
-    current_node.child = next_node
-    next_node.parent = current_node
-    @solution_nodes << next_node
+  def find_solutions
+    path = [@start]
+    find_path(path, @start)
   end
 
-  def solution_length
-    @solution_nodes.size
-  end
-end
+  private
 
-class Node
-  attr_accessor :child, :parent
+  def find_path(path, current_position)
+    path = path.dup
+    DIRECTIONS.each do |move|
+      next_position = [current_position[0] + move[0], current_position[1] + move[1]]
+      next if illegal_move?(path, next_position)
+      path << next_position
 
-  def initalize(x:, y:)
-    @x = x
-    @y = y
-  end
-
-  def ==(node)
-    if @x == node.x && @y == node.y
-      true
-    else
-      false
+      if finish?(next_position)
+        @solutions << path
+        break
+      end
+      find_path(path, next_position)
     end
+  end
+
+  def wall?(position)
+    return true if @maze[position[0]][position[1]] == '*'
+    false
+  end
+
+  def in_path?(path, position)
+    path.include?(position) ? true : false
+  end
+
+  def out_of_boundry?(next_position)
+    next_position[0] < 0 || next_position[1] < 0
+  end
+
+  def finish?(position)
+    position == @finish ? true : false
+  end
+
+  def illegal_move?(path, next_position)
+    out_of_boundry?(next_position) || wall?(next_position) || in_path?(path, next_position)
   end
 end
---
+++ labirynth/test_labyrinth.rb
@@ -0,0 +1,36 @@
+require 'minitest/autorun'
+require './labyrinth'
+
+class LabyrinthTest < Minitest::Test
+  def test_goes_throug_straigh_line
+    labyrinth = Labyrinth.new('test1.txt')
+    solution = load_solution('solution1.txt')
+    assert_equal labyrinth.solution, solution
+  end
+
+  def test_it_detects_turns
+    labyrinth = Labyrinth.new('test2.txt')
+    solution = load_solution('solution2.txt')
+    assert_equal labyrinth.solution, solution
+  end
+
+  def test_that_it_chooses_the_shortests_path
+    labyrinth = Labyrinth.new('test4.txt')
+    solution = load_solution('solution4.txt')
+    assert_equal labyrinth.solution, solution
+  end
+
+  def test_code_eval
+    labyrinth = Labyrinth.new('test3.txt')
+    solution = load_solution('solution3.txt')
+    assert_equal labyrinth.solution, solution
+  end
+
+  def load_solution(file)
+    solution = []
+    File.open(file).each_line do |file_line|
+      solution << file_line.chop.split('')
+    end
+    solution
+  end
+end
